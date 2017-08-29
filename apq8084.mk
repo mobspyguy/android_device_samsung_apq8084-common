adfab6f7eff9df61bf112e6b94a60a74f18798e7
underscoremone <underscoremone@gmail.com>
2017-08-29 18:01:36 +0100

    apq8084-common: Import common media configs
    Change-Id: I255a3b5ea8667476e7a07b08bd45376559295ec5

--- apq8084.mk
+++ apq8084.mk
@@ -28,6 +28,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.APQ8084
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
 # Radio
 PRODUCT_PACKAGES += \
     libshim_ril
